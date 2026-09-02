f706885cd5cc484283ce621e6e03dec3356b04e7
Maira <1912maira@gmail.com>
2026-09-02 13:27:19 -0700

    Add files via upload

---
+++ 14-servo motor/servo_motor.ino
@@ -0,0 +1,20 @@
+#include <Servo.h>
+Servo myservo;
+
+void setup() {
+  myservo.attach(10);
+}
+
+void loop() {
+  for(int i=0;i<=180;i++)
+  {
+    myservo.write(i);
+    delay(5);
+  }
+
+  for(int i=180;i>=0;i--)
+  {
+    myservo.write(i);
+    delay(5);
+  }
+}
